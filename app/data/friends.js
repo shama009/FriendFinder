b879f0632807bbd80eb3fd096fac8465f922cc16
Shama <shamaanjum09@outlook.com>
2018-01-10 21:47:10 -0800

    added dependencies in server.js and routes to htmlroutes.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,4 @@
-var friends = [];
-friends.push({
+var friends = [{
     "name":"Ahmed",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores":[
@@ -14,4 +13,6 @@ friends.push({
         4,
         1
       ]
-  });
+  }];
+
+  module.exports = friends;
