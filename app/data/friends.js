e12d6b47decc3b00f28c540509f62a2a109aa6a8
Shama <shamaanjum09@outlook.com>
2018-01-11 20:42:50 -0800

    logic to calculate difference of scores

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,17 +1,49 @@
 var friends = [{
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name":"Bart Simpson",
+    "photo":"http://list.lisimg.com/image/1746623/500.jpg",
     "scores":[
-        5,
         1,
-        4,
-        4,
-        5,
+        1,
+        1,
+        3,
+        2,
         1,
         2,
+        1,
+        2,
+        1
+      ]
+  },
+  {
+    "name":"Charlie Brown",
+    "photo":"https://vignette.wikia.nocookie.net/peanuts/images/b/b6/Charlie_Brown_smiling.png/revision/latest?cb=20170723213539",
+    "scores":[
+        2,
+        3,
+        2,
+        3,
+        2,
+        3,
+        2,
+        3,
+        4,
+        2
+      ]
+  },
+  {
+    "name":"Mickey Mouse",
+    "photo":"https://upload.wikimedia.org/wikipedia/en/d/d4/Mickey_Mouse.png",
+    "scores":[
+        5,
+        3,
+        4,
+        4,
+        3,
         5,
         4,
-        1
+        5,
+        5,
+        4
       ]
   }];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -31,9 +31,39 @@ module.exports = function(app) {
 
   app.post("/api/friends", function(req, res) {
     // req.body is available since we're using the body-parser middleware
-
-      friendsData.push(req.body);
-      res.json(true);
+      var surveyData = req.body;
+      
+      var difference = [];
+      // use the comparision logic from survey response and send appropriate friendData in response
+      // loop through friendsData array and get each of it's object's score values to compare with surveyData object's score values
+      // compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the totalDifference
+      // get total difference with each person's data from friendsData array and fnd the one with min totalDifference value to find best match
+    for(var i = 0; i<friendsData.length; i++) {
+        var totalDifference = 0;
+        var diffObj = {};
+        for(var j = 0; j<friendsData[i].scores.length; j++) {
+            totalDifference += Math.abs(friendsData[i].scores[j] - surveyData.scores[j]); 
+        }
+        diffObj.id = i;
+        diffObj.total = totalDifference;
+        difference.push(diffObj);
+    }
+      // Now find the minimum number from difference array's total value that is find min value in array of objects
+      var lowest = Number.POSITIVE_INFINITY;
+      var tmp;
+      var index = 0;
+      console.log(difference);
+      for (var i=difference.length-1; i>=0; i--) {
+          tmp = difference[i].total;
+          
+          if (tmp < lowest) {
+              lowest = tmp;
+              index = i;
+          }
+      }
+      // now add new surveyData to friendsData
+      friendsData.push(surveyData);
+      res.json(friendsData[index]);
 
   });
 };
